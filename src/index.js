902dd5e1aebfa55e5e0b01a76c15849ed91e1933
robmadole <robmadole@gmail.com>
2021-10-28 09:08:18 -0500

    Adding Font Awesome and repro. test case

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-window.alert('It worked!')
+import { dom, library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
+dom.i2svg()
+dom.watch()
